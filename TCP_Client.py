fb8cef0e3386f8963263e43da5d10260ffbde087
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-10-08 14:02:59 -0400

    first commit

---
+++ TCP_Client.py
@@ -0,0 +1,5 @@
+from socket import *
+
+server_name = 'localhost'
+server_port = 55555
+client_socket = socket(AF_INET, SOCK_STREAM)
---
+++ TCP_Server.py
@@ -0,0 +1,46 @@
+from socket import *
+
+def handle_request(data):
+    try:
+        segments = data.split()
+        if len(segments) != 3:
+            return "630 -1"
+        
+        oc, num1, num2 = segments
+        if oc not in ['+', '-', '*', '/']:
+            return "630 -1"
+        num1 = int(num1)
+        num2 = int(num2)
+
+        if oc == '/' and num2 == 0:
+            return "631 -1"
+        
+        if oc == '+':
+            res = num1 + num2
+        elif oc == '-':
+            res = num1 - num2
+        elif oc == '*':
+            res = num1 * num2
+        elif oc == '/':
+            res = num1 / num2
+        
+        return f"200 {res}"
+    except ValueError:
+        return "630 -1"
+
+server_port = 55555
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind(('', server_port))
+server_socket.listen(1)
+print('The server is ready to receive on port ', server_port)
+while True:
+    try: 
+        client_socket, addr = server_socket.accept()
+
+        data = client_socket.recv(1024).decode()
+        response = handle_request(data)
+        client_socket.send(data.encode())
+        client_socket.close()
+    except KeyboardInterrupt:
+        server_socket.close()
+        break
